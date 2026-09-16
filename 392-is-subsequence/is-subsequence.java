a89bc8f5aa3589d1aa57866ba214ce9db331e79d
NAGU CHAVALA <23A81A0513@sves.org.in>
2026-09-16 10:42:27 +0530

    Time: 1 ms (95.54%) | Memory: 42.9 MB (45.52%) - LeetSync

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -1,11 +1,14 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        int indS = 0;
-        if(s.length() < 1) return true;
-        for (int i = 0; i < t.length(); i++) {
-            if (s.charAt(indS) == t.charAt(i)) indS++;
-            if (indS == s.length()) return true;
-
+        int i = 0, j = 0;
+        if (s.length() == 0) return true;
+        for (i = 0; i < t.length(); i++) {
+            if (t.charAt(i) == s.charAt(j)) {
+                j++;
+                if (j == s.length()) {
+                    return true;
+                }
+            }
         }
 
         return false;
