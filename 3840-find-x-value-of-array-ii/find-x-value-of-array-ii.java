80eba702bfec0bfc3134c4a24b2be0f11dd17ed6
NAGU CHAVALA <23A81A0513@sves.org.in>
2026-09-22 17:52:05 +0530

    Time: 275 ms (26.32%) | Memory: 281.5 MB (42.10%) - LeetSync

---
+++ 3840-find-x-value-of-array-ii/find-x-value-of-array-ii.java
@@ -0,0 +1,143 @@
+class Solution {
+
+    static class Node {
+        int prod;
+        int[] cnt;
+
+        Node(int k) {
+            cnt = new int[k];
+        }
+    }
+
+    int k;
+    Node[] tree;
+
+    public int[] resultArray(int[] nums, int k, int[][] queries) {
+
+        this.k = k;
+
+        int n = nums.length;
+
+        tree = new Node[4 * n];
+
+        build(1, 0, n - 1, nums);
+
+        int[] ans = new int[queries.length];
+
+        for (int q = 0; q < queries.length; q++) {
+
+            int index = queries[q][0];
+            int value = queries[q][1];
+            int start = queries[q][2];
+            int x = queries[q][3];
+
+            // Persistent update
+            update(1, 0, n - 1, index, value);
+
+            // Get information for [start ... n-1]
+            Node res = query(1, 0, n - 1, start, n - 1);
+
+            ans[q] = res.cnt[x];
+        }
+
+        return ans;
+    }
+
+    // Build Segment Tree
+    void build(int node, int l, int r, int[] nums) {
+
+        if (l == r) {
+
+            tree[node] = new Node(k);
+
+            tree[node].prod = nums[l] % k;
+
+            tree[node].cnt[tree[node].prod] = 1;
+
+            return;
+        }
+
+        int mid = (l + r) / 2;
+
+        build(node * 2, l, mid, nums);
+        build(node * 2 + 1, mid + 1, r, nums);
+
+        tree[node] = merge(tree[node * 2], tree[node * 2 + 1]);
+    }
+
+    // Merge two segments: left + right
+    Node merge(Node left, Node right) {
+
+        Node res = new Node(k);
+
+        // Product of entire segment
+        res.prod = (int) ((long) left.prod * right.prod % k);
+
+        // Prefixes completely inside left
+        for (int r = 0; r < k; r++) {
+            res.cnt[r] += left.cnt[r];
+        }
+
+        // Prefixes that contain all of left + prefix of right
+        for (int r = 0; r < k; r++) {
+
+            if (right.cnt[r] == 0) {
+                continue;
+            }
+
+            int newRem = (int) ((long) left.prod * r % k);
+
+            res.cnt[newRem] += right.cnt[r];
+        }
+
+        return res;
+    }
+
+    // Point update
+    void update(int node, int l, int r, int index, int value) {
+
+        if (l == r) {
+
+            tree[node] = new Node(k);
+
+            tree[node].prod = value % k;
+
+            tree[node].cnt[tree[node].prod] = 1;
+
+            return;
+        }
+
+        int mid = (l + r) / 2;
+
+        if (index <= mid) {
+            update(node * 2, l, mid, index, value);
+        } else {
+            update(node * 2 + 1, mid + 1, r, index, value);
+        }
+
+        tree[node] = merge(tree[node * 2], tree[node * 2 + 1]);
+    }
+
+    // Query range [ql, qr]
+    Node query(int node, int l, int r, int ql, int qr) {
+
+        if (ql <= l && r <= qr) {
+            return tree[node];
+        }
+
+        int mid = (l + r) / 2;
+
+        if (qr <= mid) {
+            return query(node * 2, l, mid, ql, qr);
+        }
+
+        if (ql > mid) {
+            return query(node * 2 + 1, mid + 1, r, ql, qr);
+        }
+
+        Node left = query(node * 2, l, mid, ql, qr);
+        Node right = query(node * 2 + 1, mid + 1, r, ql, qr);
+
+        return merge(left, right);
+    }
+}
