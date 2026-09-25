ca1c0c6958b3e39c134d3dbf734d0da5289bc3d6
NAGU CHAVALA <23A81A0513@sves.org.in>
2026-09-25 22:16:11 +0530

    Time: 1 ms (59.85%) | Memory: 43.3 MB (32.38%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,46 +1,14 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        
-        Arrays.sort(strs, (a, b) -> Integer.compare(b.length(), a.length()));
+        Arrays.sort(strs);
+        int n = strs.length;
+        int fl = strs[0].length(), ll = strs[n - 1].length();
 
-        int m = strs.length, n = -1; 
-        for (int i = 0; i < m; i++) {
-            n = Math.max(n, strs[i].length());
+        int i = 0;
+        while (i < fl && i < ll && strs[0].charAt(i) == strs[n - 1].charAt(i)) {
+            i++;
         }
 
-        char mat[][] = new char[m][n];
-
-        for (int i = 0; i < m; i++) {
-
-            String str = strs[i];
-            char[] chars = str.toCharArray();
-
-            for (int j = 0; j < chars.length; j++) {
-
-                mat[i][j] = chars[j];
-            }
-        }
-
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < strs[0].length(); i++) {
-            
-            boolean match = true;
-            char ch = strs[0].charAt(i);
-            for (int j = 0; j < m; j++) {
-
-                if (mat[j][i] != ch) {
-                    match = false;
-                }
-            }
-
-            if (match) {
-                sb.append(ch);
-            } else {
-                break;
-            }
-        }
-
-        return sb.toString();
+        return strs[0].substring(0, i);
     }
 }
